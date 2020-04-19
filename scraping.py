fbfe2c70bc7a59728862422c0600d30e12856cf9
itasahobby <itasahobby@gmail.com>
2020-04-19 03:24:19 +0200

    Added interactivity to open products on browser

--- scraping.py
+++ scraping.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 
 import requests
+import webbrowser
 from bs4 import BeautifulSoup
 import argparse
 from tabulate import tabulate
 from printy import printy
+from printy import inputy
 
 def get_parser():
     parser = argparse.ArgumentParser(description="Scrape products from pccomponentes")
     parser.add_argument('-s',action="store", dest="search", help="Query to search from pccomponentes")
     parser.add_argument('-p',action="store_true", dest="print", help="Prints the results")
+    parser.add_argument('-i',action="store_true", dest="interactive", help="Makes an interactive shell")
     return parser
-
-
 
 class Product:
     def __init__(self, name, url, brand, price):
@@ -30,12 +31,16 @@ class Product:
         }
     def getName(self):
         return self.__name
+    def getUrl(self):
+        return self.__url
     def getBrand(self):
         return self.__brand
     def getPrice(self):
         return self.__price
     def setName(self,name):
         self.__name = name
+    def setUrl(self,url):
+        self.__url = url
     def setBrand(self,brand):
         self.__brand = brand
     def setPrice(self,price):
@@ -56,6 +61,16 @@ def scrape(target):
     products_parsed = list(map(lambda product: Product(product["data-name"],target + product["href"],product["data-brand"],product["data-price"]),products))
     return products_parsed
 
+def open_link(products):
+    val = inputy("Enter a product number to open: ","b")
+    val_int = int(val)
+    if (val_int > 0) or (val_int < len(products)):
+        printy(f"Opening link {products[val_int].getUrl()}","y")
+        webbrowser.open(products[val_int].getUrl())
+        return True
+    else:
+        return False
+
 def main():
     parser = get_parser()
     args = parser.parse_args()
@@ -65,5 +80,7 @@ def main():
     products = scrape(target)
     if(args.print):
         printTable(products)
+    if(args.interactive and args.print):
+        open_link(products)
 
 main()
